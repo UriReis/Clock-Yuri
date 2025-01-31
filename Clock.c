e70257503e9871398b69acc25fb34a0387376ccc
UriReis <uri19926@gmail.com>
2025-01-31 15:16:08 -0300

    Adicionando o semaforo e o diagrama json

--- Clock.c
+++ Clock.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "pico/stdlib.h"   
-#include "hardware/timer.h" 
+#include "pico/stdlib.h"
+#include "hardware/timer.h"
 #include "hardware/pio.h"
 #include "pio.h"
 
@@ -11,29 +11,45 @@ bool led_onR = false;
 bool led_onY = false;
 bool led_onG = false;
 
-
-bool repeating_timer_callback(struct repeating_timer *t) {
-    // Imprime uma mensagem na saída serial indicando que 1 segundo se passou.
-    printf("1 segundo passou\n");
-    //Liga ou desliga o led.
-    led_onR = !led_onR;
-    gpio_put(PIN_RED,led_onR);
-    // Retorna true para manter o temporizador repetindo. Se retornar false, o temporizador para.
+int iterador = 0;
+bool repeating_timer_callback(struct repeating_timer *t)
+{
+    switch (iterador)
+    {
+    case 0:
+        gpio_put(PIN_RED, 1); // Vermelho on
+        gpio_put(PIN_Y, 0);   // Amarelo off
+        gpio_put(PIN_G, 0);   // Verde off
+        iterador++;
+        break;
+    case 1:
+        gpio_put(PIN_RED, 0); // Vermelho off
+        gpio_put(PIN_Y, 1);   // Amarelo on
+        gpio_put(PIN_G, 0);   // Verde off
+        iterador++;
+        break;
+    case 2:
+        gpio_put(PIN_RED, 0); // Vermelho off
+        gpio_put(PIN_Y, 0);   // Amarelo off
+        gpio_put(PIN_G, 1);   // Verde on
+        iterador = 0;         // Reinicia o ciclo
+        break;
+    }
     return true;
 }
 
-int main() {
+int main()
+{
     // Inicializa a comunicação serial, permitindo o uso de funções como printf.
     stdio_init_all();
 
     // Inicializar o pino GPIO11
     gpio_init(PIN_RED);
-    gpio_set_dir(PIN_RED,true);
+    gpio_set_dir(PIN_RED, true);
     gpio_init(PIN_Y);
-    gpio_set_dir(PIN_Y,true);
+    gpio_set_dir(PIN_Y, true);
     gpio_init(PIN_G);
-    gpio_set_dir(PIN_G,true);
-
+    gpio_set_dir(PIN_G, true);
 
     // Declaração de uma estrutura de temporizador de repetição.
     // Esta estrutura armazenará informações sobre o temporizador configurado.
@@ -43,10 +59,11 @@ int main() {
     add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
     // Loop infinito que mantém o programa em execução.
-    while (true) {
-        // Pausa de 10 segundos para reduzir o uso da CPU.
-        sleep_ms(10000);
-        printf("rotina de repetição\n");
+    while (true)
+    {
+        // Pausa de 1 segundos para reduzir o uso da CPU.
+        sleep_ms(1000);
+        printf("1 segundo\n");
     }
     return 0;
 }
